93e1cbd7ee76b3925ff17181b37c5370458eb812
Sunny Srivastava <55740008+SunnySrivastava1984@users.noreply.github.com>
2025-09-04 21:07:59 +0530

    Error code as enum (#660)
    * Define error codes for json_utility API's
    
    This commit defines error codes for the errors that can be thrown by
    json_utility APIs.
    
    Change-Id: If783c6389d14fcba45662facfc98574643d7fe5c
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>
    
    * API to get error code message
    
    Defines an API to get error message based on the error code returned.
    
    Change-Id: I0cda9ac25b9176c98aec7a643d0b47aa9d665f2b
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>
    
    * Defining error codes as enum
    
    The error codes has been moved under enum because of following benefit.
    As the number against error codes were not specifying anything. Overhead
    of maintaining unique number for each error code goes away.
    
    Change-Id: Id2133c85306c7b65bb3a0ac1d622dda813f2256b
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    
    ---------
    
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Co-authored-by: Rekha Aparna <vrekhaaparna@ibm.com>

---
+++ vpd-manager/include/error_codes.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+namespace vpd
+{
+enum error_code
+{
+    // File exceptions
+    FILE_NOT_FOUND = 2001, /*Just a random value*/
+    FILE_ACCESS_ERROR,
+    EMPTY_FILE,
+
+    // JSON exceptions
+    INVALID_JSON,
+    MISSING_FLAG,
+    MISSING_ACTION_TAG,
+    FRU_PATH_NOT_FOUND,
+    JSON_PARSE_ERROR,
+    JSON_MISSING_GPIO_INFO,
+
+    // Generic errors.
+    INVALID_INPUT_PARAMETER,
+    DEVICE_NOT_PRESENT,
+    DEVICE_PRESENCE_UNKNOWN,
+    GPIO_LINE_EXCEPTION
+};
+
+const std::unordered_map<int, std::string> errorCodeMap = {
+    {error_code::FILE_NOT_FOUND, "File does not exist."},
+    {error_code::FILE_ACCESS_ERROR, "Failed to access the file."},
+    {error_code::EMPTY_FILE, "Empty file."},
+    {error_code::INVALID_JSON,
+     "Either JSON is missing FRU tag or invalid JSON object."},
+    {error_code::MISSING_FLAG,
+     "JSON is missing the flag to procees for the FRU."},
+    {error_code::MISSING_ACTION_TAG,
+     "JSON is missing the action tag to be performed for the FRU."},
+    {error_code::FRU_PATH_NOT_FOUND, "The FRU path is not found in the JSON."},
+    {error_code::JSON_PARSE_ERROR, "Error while parsing JSON file."},
+    {error_code::INVALID_INPUT_PARAMETER,
+     "Either one of the input parameter is invalid or empty."},
+    {error_code::JSON_MISSING_GPIO_INFO, "JSON missing required GPIO info."},
+    {error_code::DEVICE_NOT_PRESENT,
+     "Presence pin read successfully but device was absent."},
+    {error_code::DEVICE_PRESENCE_UNKNOWN, "Exception on presence line GPIO."},
+    {error_code::GPIO_LINE_EXCEPTION, "There was an exception in GPIO line."}};
+} // namespace vpd
